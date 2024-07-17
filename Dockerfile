ed5562d341e398350a679fa90d362aa891414d36
elianrenteria <elianrenteriadevelopment@gmail.com>
2024-07-17 14:14:58 -0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use an official Python runtime as a parent image
+FROM python:3.9-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the requirements.txt file into the container at /app
+COPY requirements.txt /app/
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Expose port 8119
+EXPOSE 8119
+
+# Run the FastAPI application
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8119"]
---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+import json
+import ollama
+app = FastAPI()
+
+
+def parse_json_from_string(string_with_json):
+    # Find where the JSON starts
+    start_index = string_with_json.find('{')
+
+    if start_index == -1:
+        raise ValueError("No JSON object found in the string")
+
+    # Find where the JSON ends
+    end_index = string_with_json.rfind('}') + 1
+
+    if end_index == 0:
+        raise ValueError("Invalid JSON format: No closing '}' found")
+
+    # Extract the JSON substring
+    json_string = string_with_json[start_index:end_index]
+
+    # Parse the JSON string into a Python dictionary
+    parsed_json = json.loads(json_string)
+
+    return parsed_json
+
+@app.get("/api")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/api/trivia")
+async def generate_question():
+    response = ollama.generate(model='llama3', prompt="""Generate me a random trivia question with the correct answer and 3 false answers and respond ONLY in json format as given here: {"question":"","answers":["","", "", ""]} for the answers value it should be an array where the first index is the correct answer.""")
+    return parse_json_from_string(response["response"])
+
