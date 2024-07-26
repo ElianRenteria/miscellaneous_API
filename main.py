03a2389640358b83705ab1ed6d1aee735d7da3aa
Elian Renteria <elianrenteriadevelopment@gmail.com>
2024-07-25 18:34:02 -0700

    fixed pipeline

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import json, requests
 from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+open_weather_key = os.getenv('OPEN_WEATHER_KEY')
+generate_api_url = os.getenv('GENERATE_API_URL')
 
 app = FastAPI()
 
@@ -56,24 +64,24 @@ async def root():
 
 @app.get("/api/trivia")
 async def generate_question():
-    response = requests.post("http://elianrenteria.net:8000/generate", json={"message": "Generate me a random trivia question with the correct answer and 3 false answers and respond ONLY in json format as given here: {\"question\":\"\",\"answers\":[\"\",\"\", \"\", \"\"]} for the answers value it should be an array where the first index is the correct answer."})
+    response = requests.post(generate_api_url, json={"message": "Generate me a random trivia question with the correct answer and 3 false answers and respond ONLY in json format as given here: {\"question\":\"\",\"answers\":[\"\",\"\", \"\", \"\"]} for the answers value it should be an array where the first index is the correct answer."})
     return parse_json_from_string(response.json()["response"])
 
 @app.post("/api/trivia")
 async def generate_question(request: MessageRequest):
     category = request.category
     message = "Generate me a random trivia question with the correct answer and 3 false answers, The Topic should be " + category + " and respond ONLY in json format as given here: {\"question\":\"\",\"answers\":[\"\",\"\", \"\", \"\"]} for the answers value it should be an array where the first index is the correct answer."
-    response = requests.post("http://elianrenteria.net:8000/generate", json={"message": message})
+    response = requests.post(generate_api_url, json={"message": message})
     return parse_json_from_string(response.json()["response"])
 
 @app.get("/api/weather")
 async def get_weather(request: WeatherRequest):
     try:
         city = request.city
-        geolocation = requests.get("http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=1&appid=f50190f583ecbc5dc7d125322da65fbe")
+        geolocation = requests.get("http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=1&appid="+str(open_weather_key))
         lat = geolocation.json()[0]["lat"]
         lon = geolocation.json()[0]["lon"]
-        weather = requests.get("https://api.openweathermap.org/data/2.5/weather?lat="+str(lat)+"&lon="+str(lon)+"&appid=f50190f583ecbc5dc7d125322da65fbe")
+        weather = requests.get("https://api.openweathermap.org/data/2.5/weather?lat="+str(lat)+"&lon="+str(lon)+"&appid="+str(open_weather_key))
         data = weather.json()
         return { "weather": data["main"], "wind": data["wind"], "misc": data["weather"] }
     except():
