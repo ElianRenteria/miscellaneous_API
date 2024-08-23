9807fd8051ec88a657acc9d7f94c1aa58da0bd96
Elian Renteria <elianrenteriadevelopment@gmail.com>
2024-08-22 18:09:14 -0700

    added weather to cors

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ origins = [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
     "https://coderlab.work",
-    "https://elianrenteria.github.io/weather/"
+    "https://elianrenteria.github.io"
     # Add other origins as needed
 ]
 
