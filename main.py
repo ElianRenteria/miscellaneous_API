3199bccd229bdac141d7c3922ee3775c8c60fff1
elianrenteria <elianrenteriadevelopment@gmail.com>
2025-01-16 16:03:25 -0800

    fixed starbucks data return type to json

--- main.py
+++ main.py
@@ -289,4 +289,4 @@ data = pd.read_csv(path)
 async def get_random_starbucks_drink():
     # Pick a new random row each time the endpoint is called
     random_row = data.sample(n=1)
-    return json.loads(random_row.to_json(orient='records'))  # Use 'records' for better formatting
+    return {"drink": json.loads(random_row.to_json(orient='records'))[0]}  # Use 'records' for better formatting
